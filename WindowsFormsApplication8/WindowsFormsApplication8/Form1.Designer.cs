c1146a5d76da0053a79bf97b6a3e6643aa748f3d
Andrew Hua (花毓禧) <andrew.hua@pomerobotservice.com>
2018-06-13 13:47:08 +0800

    滑鼠鎖點範例

---
+++ WindowsFormsApplication8/WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(40, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(458, 324);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 376);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 596);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication8/WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        Point s;
+        Point end;
+        bool isDrag = false;
+        Point startPoint;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            s = new Point(10, 10);
+            end = new Point(100, 100);
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawLine(Pens.Black, s, end);
+            pictureBox1.Image = bmp;
+
+            Form f = new Form();
+            label1.Text = f.Location.X.ToString() + "\n" + f.Location.Y.ToString();
+        }
+
+        private void MoveCursor()
+        {
+            // Set the Current cursor, move the cursor's Position,
+            // and set its clipping rectangle to the form. 
+
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50);
+            Cursor.Clip = new Rectangle(this.Location, this.Size);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrag = true;
+            }
+
+            Control control = (Control)sender;
+            Form f = new Form();
+            // Calculate the startPoint by using the PointToScreen 
+            // method.
+            startPoint = control.PointToScreen(new Point(f.Location.X, f.Location.Y));
+            label1.Text = startPoint.X.ToString() + "\n" + startPoint.Y.ToString();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)//鎖點範例
+        {
+            Control control = (Control)sender;
+            Form f = new Form();
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            if ((e.X - s.X < 10 && e.X - s.X > -10) && (e.Y - s.Y < 10 && e.Y - s.Y > -10))//滑鼠靠近s
+            {
+                startPoint = control.PointToScreen(new Point(f.Location.X, f.Location.Y));
+                startPoint.X += s.X;
+                startPoint.Y += s.Y;
+                Cursor.Position = startPoint;
+                //Cursor.Clip = new Rectangle(this.Location, this.Size);
+            }
+        }
+    }
+}
